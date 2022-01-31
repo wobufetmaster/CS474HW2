136e082f9702796dc8fe1ae2739b2567a4dd62a5
Sean Stiely <wobufetmaster@gmail.com>
2022-01-31 11:17:16 -0600

    DSL class is better

--- src/main/scala/MySetTheoryDSL.scala
+++ src/main/scala/MySetTheoryDSL.scala
@@ -3,14 +3,28 @@ object MySetTheoryDSL:
 
   enum setExp:
     case Value(input: BasicType)
-    case Var(name: String)
-    case Add(op1: setExp, op2: setExp)
-    case Sub(op1: setExp, op2: setExp)
-    private val bindingScoping: Map[String, Int] = Map("x"->2, "Adan"->10)
-    def eval: BasicType = {1}
+    case Variable(name: String)
+    case Check(op1: setExp, op2: BasicType)
+    case Macro(name: String)
+    case Scope()
+    case Assign(name: String, op2: setExp)
+    case Insert(name: String, op2: BasicType)
+    case Delete(name: String, op2: BasicType)
+    case Union(op1: setExp, op2: setExp)
+    case Intersection(op1: setExp, op2: setExp)
+    case Difference(op1: setExp, op2: setExp)
+    case SymmetricDifference(op1: setExp, op2: setExp)
+    case Product(op1: setExp, op2: setExp)
+
+    private val bindingScoping: Map[String, BasicType] = Map("x"->2, "Adan"->10)
+
+    def eval: BasicType = {
+      this match
+        case default => 0
+    }
 
   @main def runSetExp(): Unit =
     import setExp.*
     println("Hello world!")
-    val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
-    println(firstExpression)
+    //val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
+    //println(firstExpression)
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class Test1 extends AnyFunSuite {
   test("Default test") {
     import setExp.*
-    val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
+    //val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
     println("Running test 1!")
     assert(true)
   }
