579efb6bd07422c330eaf7bb13f1354ecddae8fc
wobufetmaster <60907338+wobufetmaster@users.noreply.github.com>
2022-02-04 12:18:02 -0600

    Updated gitignore

--- src/main/scala/MySetTheoryDSL.scala
+++ src/main/scala/MySetTheoryDSL.scala
@@ -1,3 +1,5 @@
+import MySetTheoryDSL.setExp.Variable
+
 object MySetTheoryDSL:
   type BasicType = Int
 
@@ -7,24 +9,20 @@ object MySetTheoryDSL:
     case Check(op1: setExp, op2: BasicType)
     case Macro(name: String)
     case Scope()
-    case Assign(name: String, op2: setExp)
-    case Insert(name: String, op2: BasicType)
-    case Delete(name: String, op2: BasicType)
+    case Assign(name: Variable, op2: Seq[setExp])
+    case Insert(name: Variable, op2: BasicType)
+    case Delete(name: Variable, op2: BasicType)
     case Union(op1: setExp, op2: setExp)
     case Intersection(op1: setExp, op2: setExp)
     case Difference(op1: setExp, op2: setExp)
     case SymmetricDifference(op1: setExp, op2: setExp)
     case Product(op1: setExp, op2: setExp)
-
-    private val bindingScoping: Map[String, BasicType] = Map("x"->2, "Adan"->10)
-
-    def eval: BasicType = {
-      this match
-        case default => 0
-    }
+    def check: Boolean = {false}
 
   @main def runSetExp(): Unit =
     import setExp.*
     println("Hello world!")
-    //val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
+    //val expression = Assign(Variable("someSetName"), Insert(Variable("var"), Value(1)), Value(1))
+    //val expression2 = Assign(SetIdentity("someSetName"),Seq( Variable("var"), Value(1), Value(1)))
+//val firstExpression = Sub(Add(Add(Value(2), Value(3)),Var("Adan")), Var("x")).eval
     //println(firstExpression)
