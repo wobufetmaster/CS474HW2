6d9e5de1d3cc3b80f6b5b55959265394f5a665dc
wobufetmaster <60907338+wobufetmaster@users.noreply.github.com>
2022-02-07 22:29:16 -0600

    Added more functionality

--- src/main/scala/MySetTheoryDSL.scala
+++ src/main/scala/MySetTheoryDSL.scala
@@ -14,14 +14,13 @@ object MySetTheoryDSL:
     case Macro(name: String)
     case Scope(name: String, op2:setExp)
     case Assign(name: Variable, op2: setExp)
-    case Insert(name: Variable, op2: BasicType*)
-    case Delete(name: Variable, op2: BasicType)
+    case Insert(op: setExp*)
+    case Delete(name: Variable)
     case Union(op1: setExp, op2: setExp)
     case Intersection(op1: setExp, op2: setExp)
     case Difference(op1: setExp, op2: setExp)
     case SymmetricDifference(op1: setExp, op2: setExp)
     case Product(op1: setExp, op2: setExp)
-    case NewSet()
 
     def eval(): Set[Any] = { //Walks through the AST and returns a set.
       this match {
@@ -39,7 +38,13 @@ object MySetTheoryDSL:
               scope_map.updated((s,"scope1"),set.eval())
           }
           Set()
-        case Insert(set_name, to_insert*) => set_name.eval() | to_insert.toSet
+        case Insert(to_insert*) => to_insert.foldLeft(Set())((v1,v2) => v1 | v2.eval())
+        case Delete(name) => Set()
+        case Union(op1, op2) => op1.eval() + op2.eval()
+        case Intersection(op1, op2) => op1.eval() & op2.eval()
+        case Difference(op1, op2) => op1.eval() -- op2.eval()
+        case SymmetricDifference(op1, op2) => op1.eval() &~ op2.eval()
+        case Product(op1, op2) => op1.eval() + op2.eval()
 
 
         case default => Set()
@@ -56,9 +61,10 @@ object MySetTheoryDSL:
     import setExp.*
 
     //println("Hello world!")
-    println(Assign(Variable("someSetName"), Insert(Variable("var"), 2,3,5,7)).eval())
+    println(Insert(Value(2),Value(3),Value(5),Value(7)).eval())
+    //println(Assign(Variable("someSetName"), Insert(Value(2),Value(3),Value(5),Value(7))).eval())
 
-    println(Insert(Variable("set1"),7,9).eval())
+    //println(Insert(Variable("set1"),7,9).eval())
     println(Check("set1",Value(5)))
     //Scope("scopename", Scope("othername", Assign(Variable("someSetName"), Insert(Variable("var"), Value(1)), Value("somestring"))))
 
